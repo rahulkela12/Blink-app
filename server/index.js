9fecc6ab803fdf314370b8d5d14358d7ee185a54
rahul@LAPTOP-7O1THPFP.(none) <rahulkela1234@gmail.com>
2023-07-03 11:58:35 +0530

    Intial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+//import express from 'express';
+const cors = require("cors");
+const mongoose = require('mongoose');
+const userRoutes =require("./routes/userRoutes");
+
+const app = express();
+require("dotenv").config();
+
+
+//const server = app.listen();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",userRoutes);
+
+mongoose.connect(process.env.MONGO_URL,{
+     useNewUrlParser : true,
+     useUnifiedTopology : true,
+}).then(()=>{
+  console.log("DB connect successfully");
+}).catch((err)=>{
+     console.log(err);
+});
+
+const server = app.listen(process.env.PORT,() =>{
+     console.log("Server Started on Port" + " "+process.env.PORT);
+})
