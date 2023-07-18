aa6e9559b1fc16c0bccb256a150f6c48c55b5536
rahul@LAPTOP-7O1THPFP.(none) <rahulkela1234@gmail.com>
2023-07-18 18:31:17 +0530

    Chat setup and welcome page

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import styled from 'styled-components';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import  {allUsersRoute}  from '../utils/APIRoutes';
+import Contacts from '../components/Contacts';
+import Welcome from '../components/Welcome';
+import ChatContainer from '../components/ChatContainer';
 
 function Chat() {
+  const navigate = useNavigate();
+  const[contacts,setContacts] = useState([]);
+  const[currentUser,setCurrentUser] = useState(undefined);
+  let[currentChat,setCurrentChat] = useState(undefined);
+  const [isLoaded,setIsLoaded] = useState(false);
+  useEffect(() =>{
+    const callback = async()=> {if(!localStorage.getItem('chat-app-user')){
+      navigate('/login');
+    }else{
+      setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')));
+      setIsLoaded(true);
+    }}
+    callback();
+  },[])
+  useEffect(() => {
+    const callback = async()=> {
+       if(currentUser){
+        if(currentUser.isAvatarImageSet){
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        }else{
+          navigate("/setAvatar");
+        }
+       }
+    }
+    callback();
+  },[currentUser]);
+  const handleChatChange = (chat) => 
+    {setCurrentChat = {chat};}
   return (
     <Container>
       <div className='container'>
-
+        <Contacts 
+        contacts={contacts} 
+        currentUser={currentUser} 
+        changeChat = {handleChatChange}
+        setCurrentChat={setCurrentChat}
+        />
+        {
+         isLoaded && (currentChat === undefined) ? 
+          <Welcome 
+        currentUser = {currentUser}
+         /> : (<ChatContainer 
+          currentUser = {currentUser}
+         />)
+        }
       </div>
     </Container>
   )
